25db20f5d093b8153998294353f03bc7c728bb0f
Jared Hanson <jaredhanson@gmail.com>
2023-12-14 16:19:28 -0800

    Update to latest kerouac.

--- content/docs/install.md
+++ content/docs/install.md
@@ -1,5 +1,6 @@
 ---
 title: 'Install'
+layout: default
 ---
 
 Install kerouac by...
--- site.js
+++ site.js
@@ -1,21 +1,28 @@
 var kerouac = require('kerouac');
-var site = kerouac();
+// TODO: kerouac-cname
 
-site.set('base url', 'http://kerouacjs.org/');
+var site = kerouac();
+site.set('base url', 'https://www.kerouacjs.org');
 site.set('output', 'dist');
 
-site.engine('ejs', require('ejs'));
+//site.engine('ejs', require('ejs'));
 
-site.content('content');
+site.use(kerouac.content('content'));
 
 //site.plug(require('kerouac-sitemap')());
 //site.plug(require('kerouac-robotstxt')());
 
 
-site.generate(function(err) {
-  if (err) {
-    console.error(err.message);
-    console.error(err.stack);
-    return;
-  }
-});
+site.generate(
+  {
+    '/': [
+      kerouac.content.createMapper(),
+    ]
+  },
+  function(err) {
+    if (err) {
+      console.error(err.message);
+      console.error(err.stack);
+      return;
+    }
+  });
